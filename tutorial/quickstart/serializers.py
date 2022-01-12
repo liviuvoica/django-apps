748e316316654e988d7ef1f0a9533819aaca8d3c
Voica Liviu <liviu.voica.dev@gmail.com>
2022-01-12 18:04:36 +0200

    Django rest framework: - added comments to serializers module

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -1,13 +1,20 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+# Map the User model to the UserSerializer class
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
-
+# Map the Group model to the GroupSerializer class
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
+        fields = ['url', 'name']
+
+# Map the Customer model to the CustomerSerializer class
+class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Customer
         fields = ['url', 'name']
