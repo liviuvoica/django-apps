b656e005d7cac8d85beb251f9b969380d691df80
Voica Liviu <liviu.voica.dev@gmail.com>
2022-01-13 18:03:16 +0200

    Django rest framework improvements: - request and responses; - wrapped api views; - class based views; - rewrite the api using class based views; - mixins; - generic class based views;

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -26,6 +26,7 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
