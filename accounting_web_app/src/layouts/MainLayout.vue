95af317bf10ffbe76802d2bed3f1a801c1bfe5e4
liviuvoica <87973797+liviuvoica@users.noreply.github.com>
2022-02-16 17:07:41 +0200

    Cleaned quasar project for unnecessary files and methods

--- accounting_web_app/src/layouts/MainLayout.vue
+++ accounting_web_app/src/layouts/MainLayout.vue
@@ -10,10 +10,7 @@
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title> Accounting Web Application </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
@@ -36,7 +33,7 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue';
+import EssentialLink from '../components/EssentialLink.vue';
 
 const linksList = [
   {
@@ -50,37 +47,7 @@ const linksList = [
     caption: 'github.com/quasarframework',
     icon: 'code',
     link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
+  }
 ];
 
 import { defineComponent } from 'vue';
