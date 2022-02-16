4247e73bc5ea2119f3e049e6a81736a890e42eac
liviuvoica <87973797+liviuvoica@users.noreply.github.com>
2022-02-16 15:44:03 +0200

    Delete the entire 'tutorial' folder

--- tutorial/orders/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Order(models.Model):
-    order_id = models.IntegerField()
-    order_value = models.IntegerField()
-    order_date = models.DateTimeField(auto_now_add=True)
